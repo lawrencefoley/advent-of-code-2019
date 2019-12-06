94f4167a19f6d6f8ff3d840c9426b5610778273b
Lawrence Foley <emwav333@gmail.com>
2019-12-05 23:23:41 -0600

    Day 1 formatting

--- day01/day01.c
+++ day01/day01.c
@@ -9,65 +9,65 @@ int calcFuel(int mass) {
 int calcFuelPart2(int mass) {
 	// For part 2
 	int temp = mass / 3 - 2;
-	if(temp <= 0) {
+	if (temp <= 0) {
 		return 0;
 	}
 	else {
-		return temp + calcFuelPart2(temp);	
+		return temp + calcFuelPart2(temp);
 	}
 }
 
 int main() {
- 	printf("%s\n", "Day 1");
+	printf("%s\n", "Day 1");
 
- 	char inputFileName [] = "input.txt";
- 	FILE * inputStream;
-    inputStream = fopen(inputFileName, "r");
-    
-    // Check if file has been opened
-    if(!inputStream)
-    {
-        printf("Can't open file %s", inputFileName);
-        exit(1);   
-    }
-    
-    // Read all the ints in the file
-    int totalNums = 10;
-    int * nums;
-    nums = malloc(sizeof(int) * 10);
-    int temp = 0;
-    int index = 0;
+	char inputFileName [] = "input.txt";
+	FILE * inputStream;
+	inputStream = fopen(inputFileName, "r");
 
-    while(fscanf(inputStream, " %d", &temp) != EOF)
-    {
-    	if(index >= totalNums) {
-    		// need to increase array size
-    		int * tempArray;
-    		totalNums *= 2;
-    		tempArray = (int *)calloc(totalNums, sizeof(int));
-    		memcpy(tempArray, nums, index * sizeof(int));
-    		free(nums);
-    		nums = tempArray;
-    	}
-        nums[index] = temp;
-        index++;
-    }
-    // Close the file
-    fclose(inputStream);
+	// Check if file has been opened
+	if (!inputStream)
+	{
+		printf("Can't open file %s", inputFileName);
+		exit(1);
+	}
 
-    long total = 0;
-    int i;
-    for(i = 0; i < index; i++) {
-    	total += calcFuel(nums[i]);
-    }
+	// Read all the ints in the file
+	int totalNums = 10;
+	int * nums;
+	nums = malloc(sizeof(int) * 10);
+	int temp = 0;
+	int index = 0;
 
-    printf("Part 1: %d\n", total);
+	while (fscanf(inputStream, " %d", &temp) != EOF)
+	{
+		if (index >= totalNums) {
+			// need to increase array size
+			int * tempArray;
+			totalNums *= 2;
+			tempArray = (int *)calloc(totalNums, sizeof(int));
+			memcpy(tempArray, nums, index * sizeof(int));
+			free(nums);
+			nums = tempArray;
+		}
+		nums[index] = temp;
+		index++;
+	}
+	// Close the file
+	fclose(inputStream);
 
-    long part2Total = 0;
-    for(i = 0; i < index; i++) {
-    	part2Total += calcFuelPart2(nums[i]);
-    }
-    printf("Part 2: %ld\n", part2Total);
+	long total = 0;
+	int i;
+	for (i = 0; i < index; i++) {
+		total += calcFuel(nums[i]);
+	}
+
+	printf("Part 1: %d\n", total);
+
+	long part2Total = 0;
+	for (i = 0; i < index; i++) {
+		part2Total += calcFuelPart2(nums[i]);
+	}
+	printf("Part 2: %ld\n", part2Total);
 
 	return 0;
 }
